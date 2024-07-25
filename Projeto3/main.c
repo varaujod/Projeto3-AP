638542b43a5d0d127b3c1c071ca14421ca4b36d8
Vinicius Araújo <162530634+varaujod@users.noreply.github.com>
2024-07-25 13:30:38 -0300

    Add files via upload

---
+++ Projeto3/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	printf("V         V I	N      N I \n");
+	printf("V        V  I	N     NN I \n");
+	printf(" V     V    I	N  N   N I \n");
+	printf("  V   V     I	NN     N I \n");
+	printf("    V       I   N      N I \n");
+} 
